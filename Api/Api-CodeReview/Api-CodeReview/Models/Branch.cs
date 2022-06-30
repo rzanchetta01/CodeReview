39402f6a37aa881c4dd4d6484dab4f8818a055b7
Gabriella Martins <gabmcavalheiro641@gmail.com>
2022-06-30 17:17:17 -0300

    Instalação de dependencias e criação das Models
    Instalação das dependencias do projeto, criação das models e mapeamento com o EF

---
+++ Api/Api-CodeReview/Api-CodeReview/Models/Branch.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Api_CodeReview.Models
+{
+    [Table("tbBrach")]
+    public class Branch
+    {
+        [Key]
+        public int Id_branch { get; set; }
+        public int Id_repositorio { get; set; }
+        public string Nm_branch { get; set; }
+        public string Nm_email_dev { get; set; }
+        public string Nm_email_review { get; set; }
+
+        [ForeignKey("Id_repositorio")]
+        public Repositorio repositorio { get; set; }
+    }
+}
---
+++ Api/Api-CodeReview/Api-CodeReview/Models/Repositorio.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Api_CodeReview.Models
+{
+    [Table("tbRepositorio")]
+    public class Repositorio
+    {
+        [Key]
+        public int Id_repositorio { get; set; }
+        public string  Nm_repositorio { get; set; }
+        public string Nm_url_clone { get; set; }
+        public string Nm_usuario { get; set; }
+        public string Nm_senha { get; set; }
+        public string Nm_email_admin { get; set; }
+    }
+}
