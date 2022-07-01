dbfddc595b7ef2c306c4496bbfaecd64862e01f1
Gabriella Martins <gabmcavalheiro641@gmail.com>
2022-06-30 23:58:08 -0300

    Contexto
    Adicionando o contexto da aplicação e configuração no startup

---
+++ Api/Api-CodeReview/Api-CodeReview/Context/AppDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Api_CodeReview.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using System.IO;
+
+namespace Api_CodeReview.Context
+{
+    public class AppDbContext : DbContext
+    {
+        public DbSet<Repositorio> Repositorios { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            IConfiguration configuration = new ConfigurationBuilder()
+                .SetBasePath(Directory.GetCurrentDirectory())
+                .AddJsonFile("appsettings.json", false, false)
+                .Build();
+
+            optionsBuilder.UseSqlServer(configuration.GetConnectionString("DEV_Code_Review"));
+        }
+    }
+}
