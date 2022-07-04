539f3d69adac438d2db33ff23df8ff714025efdc
Gabriella Martins <gabmcavalheiro641@gmail.com>
2022-07-04 12:20:33 -0300

    Scaffold BranchesController
    Adicionando controlador de API de branches com ações usando o EF, por meio de um novo item scaffold

---
+++ Api/Api-CodeReview/Api-CodeReview/Controllers/BranchesController.cs
@@ -0,0 +1,108 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Api_CodeReview.Context;
+using Api_CodeReview.Models;
+
+namespace Api_CodeReview.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class BranchesController : ControllerBase
+    {
+        private readonly AppDbContext _context;
+
+        public BranchesController(AppDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Branches
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<Branch>>> GetBranchs()
+        {
+            return await _context.Branchs.ToListAsync();
+        }
+
+        // GET: api/Branches/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<Branch>> GetBranch(int id)
+        {
+            var branch = await _context.Branchs.FindAsync(id);
+
+            if (branch == null)
+            {
+                return NotFound();
+            }
+
+            return branch;
+        }
+
+        // PUT: api/Branches/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutBranch(int id, Branch branch)
+        {
+            if (id != branch.Id_branch)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(branch).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!BranchExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Branches
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<Branch>> PostBranch(Branch branch)
+        {
+            _context.Branchs.Add(branch);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetBranch", new { id = branch.Id_branch }, branch);
+        }
+
+        // DELETE: api/Branches/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteBranch(int id)
+        {
+            var branch = await _context.Branchs.FindAsync(id);
+            if (branch == null)
+            {
+                return NotFound();
+            }
+
+            _context.Branchs.Remove(branch);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool BranchExists(int id)
+        {
+            return _context.Branchs.Any(e => e.Id_branch == id);
+        }
+    }
+}
