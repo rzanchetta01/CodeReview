e0e992c2f8b1b0ad5fabc2597cb80dd34d8ef494
Gabriella Martins <gabmcavalheiro641@gmail.com>
2022-07-05 12:12:36 -0300

    Validação do modelo
    Adicionando a validação para entrada de dados no modelo SLA

--- Api/Api-CodeReview/Api-CodeReview/Models/SLA.cs
+++ Api/Api-CodeReview/Api-CodeReview/Models/SLA.cs
@@ -8,8 +8,17 @@ namespace Api_CodeReview.Models
     {
         [Key]
         public int Id_SLA { get; set; }
+
+        [Required]
+        [StringLength(1000, MinimumLength = 1)]
         public int Id_repositorio { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 1)]
         public int Nr_dias_sla_commit { get; set; }
+
+        [Required]
+        [StringLength(50, MinimumLength = 1)]
         public int Nr_dias_sla_review { get; set; }
 
         //[ForeignKey("Id_repositorio")]
