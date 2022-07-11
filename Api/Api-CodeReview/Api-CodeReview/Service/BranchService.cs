ac70d3e17d3e77c67c1d2de3c4b85481fae18178
Rodrigo Zanchetta <rzanchetta01@gmail.com>
2022-07-11 15:22:49 -0300

    Validando nome da branch e corrigindo tamanho mínimo campo nmBranch

---
+++ Api/Api-CodeReview/Api-CodeReview/Service/BranchService.cs
@@ -0,0 +1,34 @@
+﻿using Api_CodeReview.Models;
+using LibGit2Sharp;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+
+namespace Api_CodeReview.Service
+{
+    public class BranchService
+    {
+        public static string[] ListarPossiveisBranchs(Repositorio repo)
+        {
+            string urlClone = repo.Nm_url_clone[8..];
+            List<string> branchs = new();
+
+            Process process = new();
+            process.StartInfo.FileName = "powershell.exe";
+            process.StartInfo.UseShellExecute = false;
+            process.StartInfo.RedirectStandardOutput = true;
+            process.StartInfo.CreateNoWindow = true;
+            process.StartInfo.Arguments = $"git ls-remote 'https://{repo.Nm_usuario}:{repo.Nm_senha}@{urlClone}' 'refs/heads/*'";
+            process.Start();
+            StreamReader stdOut = process.StandardOutput;
+            process.WaitForExit();
+            while (!stdOut.EndOfStream)
+                branchs.Add(stdOut.ReadLine());
+
+
+            return branchs.ToArray();
+        }
+    }
+}
