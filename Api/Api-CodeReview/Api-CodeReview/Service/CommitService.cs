63ebe8e71ba7226abf850e0a843c18245f845792
Rodrigo Zanchetta <rzanchetta01@gmail.com>
2022-07-14 15:29:06 -0300

    base impl Service commit e repositorio

---
+++ Api/Api-CodeReview/Api-CodeReview/Service/CommitService.cs
@@ -0,0 +1,19 @@
+﻿using Api_CodeReview.Context;
+using Api_CodeReview.Repository.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_CodeReview.Service
+{
+    public class CommitService
+    {
+        private readonly ICommitRepository repository;
+
+        public CommitService(AppDbContext context)
+        {
+            repository = new CommitRepository(context);
+        }
+    }
+}
---
+++ Api/Api-CodeReview/Api-CodeReview/Service/RepositorioService.cs
@@ -0,0 +1,19 @@
+﻿using Api_CodeReview.Context;
+using Api_CodeReview.Repository;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Api_CodeReview.Service
+{
+    public class RepositorioService
+    {
+        private readonly IRepositorioRepository repository;
+
+        public RepositorioService(AppDbContext context)
+        {
+            repository = new RepositorioRepository(context);
+        }
+    }
+}
