9721b0817ea269f441c4b1f8d30fc6f68b4e5756
Rodrigo Zanchetta <rzanchetta01@gmail.com>
2022-07-14 14:46:26 -0300

    Branch service/ controller imp

--- Api/Api-CodeReview/Api-CodeReview/Service/BranchService.cs
+++ Api/Api-CodeReview/Api-CodeReview/Service/BranchService.cs
@@ -1,34 +1,81 @@
-﻿using Api_CodeReview.Models;
+﻿using Api_CodeReview.Context;
+using Api_CodeReview.Models;
+using Api_CodeReview.Repository;
 using LibGit2Sharp;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
 using System.Diagnostics;
 using System.IO;
 using System.Linq;
+using System.Threading.Tasks;
 
 namespace Api_CodeReview.Service
 {
     public class BranchService
     {
-        public static string[] ListarPossiveisBranchs(Repositorio repo)
+        private readonly IBranchRepository repository;
+
+        public BranchService(AppDbContext context)
         {
-            string urlClone = repo.Nm_url_clone[8..];
-            List<string> branchs = new();
+            repository = new BranchRepository(context);
+        }
+    
+        public async Task<IEnumerable<Models.Branch>> GetBranches()
+        {
+            return await repository.GetAll();
+        }
 
-            using Process process = new();
-            process.StartInfo.FileName = "powershell.exe";
-            process.StartInfo.UseShellExecute = false;
-            process.StartInfo.RedirectStandardOutput = true;
-            process.StartInfo.CreateNoWindow = true;
-            process.StartInfo.Arguments = $"git ls-remote 'https://{repo.Nm_usuario}:{repo.Nm_senha}@{urlClone}' 'refs/heads/*'";
-            process.Start();
-            using StreamReader stdOut = process.StandardOutput;
-            process.WaitForExit();
-            while (!stdOut.EndOfStream)
-                branchs.Add(stdOut.ReadLine());
+        public async Task<Models.Branch> GetBranch(int id)
+        {
+            return await repository.GetById(id);
+        }
 
-            return branchs.ToArray();
+        public async Task<Models.Branch> GetBranch(string nome)
+        {
+            return await repository.GetByNome(nome);
+        }
 
+        public async Task PutBranch(Models.Branch branch, int id)
+        {
+            if (id != branch.Id_branch)
+            {
+                return;
+            }
+
+            try
+            {
+                repository.Update(branch);
+                await repository.Save();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!repository.BranchExist(id))
+                {
+                    return;
+                }
+                else
+                {
+                    throw;
+                }
+            }
+        }
+    
+        public async Task PostBranch(Models.Branch branch)
+        {
+            await repository.Post(branch);
+        }
+
+        public async Task DeleteBranch(int id)
+        {
+            try
+            {
+                await repository.Delete(id);
+            }
+            catch (Exception)
+            {
+                throw;
+            }
         }
     }
 }
