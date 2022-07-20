72b274645d111b0e7545a9f42431ae5b5ca7d1cd
Rodrigo Zanchetta <rzanchetta01@gmail.com>
2022-07-20 13:52:18 -0300

    Camada service impl

---
+++ ExecutavelGitAnalyzer/Service/CommitService.cs
@@ -0,0 +1,93 @@
+﻿using ExecutavelGitAnalyzer.Infra.Database.Commit;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExecutavelGitAnalyzer.Service
+{
+    class CommitService
+    {
+        private readonly ICommitOperations commitOperations;
+        private readonly RepositorioService repositorioService;
+        private readonly BranchService branchService;
+
+        public CommitService(RepositorioService repositorio, BranchService branch)
+        {
+            commitOperations = new CommitOperations();
+            repositorioService = repositorio;
+            branchService = branch;
+        }
+
+        public (DateTime, string) GetLastCommitDateAndId(string branchName, string repoName)
+        {
+            try
+            {
+                if (!branchService.BranchExist(branchName))
+                    throw new Exception("BRANCH NÃO ENCONTRADA -->" + branchName + " DO REPOSITORIO ->" + repoName);
+
+                if (!repositorioService.RepositorioExist(repoName))
+                    throw new Exception("REPOSITORIO NÃO ENCONTRADO -->" + repoName);
+
+                return commitOperations.GetLastCommitDateAndId(branchName, repoName);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR : " + e.Message);
+                return (DateTime.Now, null);
+            }
+        }
+
+        public void InsertLastCommit(Models.Commit commit, int idBranch)
+        {
+
+            try
+            {
+                if (commit.IdCommit == null)
+                    throw new Exception("ID DO NOVO COMMIT EM BRANCO");
+
+                if (commit.Nm_autor == null)
+                    throw new Exception("AUTOR NOVO COMMIT EM BRANCO");
+
+                if(commit.Nm_mensagem == null)
+                    throw new Exception("MENSAGEM DO NOVO COMMIT EM BRANCO");
+
+                if (!branchService.BranchExist(idBranch))
+                    throw new Exception("ESSA BRANCH NÃO EXISTE ->" + idBranch);
+
+                commitOperations.InsertLastCommit(commit, idBranch);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR : " + e.Message);
+                return;
+            }
+        }
+
+        public void UpdateLastCommit(Models.Commit commit, string oldIdCommit)
+        {
+            try
+            {
+                if (commit.IdCommit == null)
+                    throw new Exception("ID DO NOVO COMMIT EM BRANCO");
+
+                if (commit.Nm_autor == null)
+                    throw new Exception("AUTOR NOVO COMMIT EM BRANCO");
+
+                if (commit.Nm_mensagem == null)
+                    throw new Exception("MENSAGEM DO NOVO COMMIT EM BRANCO");
+
+                if (!commitOperations.CommitExist(oldIdCommit))
+                    throw new Exception("ESSE ID COMMIT NÃO EXISTE ->" + oldIdCommit);
+
+                commitOperations.UpdateLastCommit(commit, oldIdCommit);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR : " + e.Message);
+                return;
+            }
+        }
+    }
+}
---
+++ ExecutavelGitAnalyzer/Service/CriptografiaService.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExecutavelGitAnalyzer.Service
+{
+    class CriptografiaService
+    {
+        public static string Encrypt(string input)
+        {
+            return Infra.Criptografia.Encrypt(input);
+        }
+
+        public static string Decrypt(string input)
+        {
+            return Infra.Criptografia.Decrypt(input);
+        }
+    }
+}
