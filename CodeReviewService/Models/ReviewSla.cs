9ff15be9c364f87763f8995f72eb6224699d7de1
Rodrigo Zanchetta <rzanchetta01@gmail.com>
2022-08-08 15:56:00 -0300

    SERVICE - IMPL FEEDBACK SERVICE

---
+++ CodeReviewService/Models/ReviewSla.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeReviewService.Models
+{
+    class ReviewSla
+    {
+        public string LinkRepo { get; set; }
+        public string EmailAdmin { get; set; }
+        public string EmailReview { get; set; }
+        public DateTime DtRegistro { get; set; }
+        public DateTime DtSla { get; set; }
+        public DateTime DtFeedback { get; set; }
+        public string IdCommit { get; set; }
+        public string NmBranch { get; set; }
+
+        public ReviewSla()
+        {
+
+        }
+
+        public override string ToString()
+        {
+            return $"[{LinkRepo}, {EmailAdmin}, {EmailReview}, {DtRegistro}, {DtSla}, {DtFeedback}, {IdCommit}, {NmBranch}]";
+        }
+    }
+}
---
+++ CodeReviewService/Service/FeedbackService.cs
@@ -0,0 +1,59 @@
+﻿using CodeReviewService.Infra.Database.Feedback;
+using CodeReviewService.Models;
+using Microsoft.Extensions.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeReviewService.Service
+{
+    class FeedbackService
+    {
+        private readonly IFeedbackRepositoryOperations repository;
+        public FeedbackService(ILogger logger)
+        {
+            repository = new FeedbackRepository(logger);
+        }
+        public void PostInitialBaseFeedback(string IdCommit, int IdBranch)
+        {
+            try
+            {
+                if (repository.FeedbackExist(IdCommit))
+                    throw new Exception("Ja existe um feedback para esse commit");
+
+                repository.PostFeedback(IdCommit, IdBranch);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR --> " + e.Message);
+            }
+        }
+
+        public IEnumerable<ReviewSla> GetFeedbackNotReviwed()
+        {
+            try
+            {
+                var content = repository.GetAllFeedbacks();
+
+                if (content is null)
+                    throw new Exception("Nenhum feedback encontrado");
+
+                content = content.Where(x => x.DtFeedback != DateTime.MinValue).ToList();
+
+                foreach (var item in content)
+                {
+                    Console.WriteLine(item.ToString());
+                }
+
+                return content;
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("ERROR --> " + e.Message);
+                return null;
+            }
+        }
+    }
+}
